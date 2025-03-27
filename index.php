d01452baa590df7249739eff214cd05ce4f2bec5
Neo Poly Works <contact.edwyn@gmail.com>
2025-03-27 01:50:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -503,10 +503,10 @@ include 'includes/header.php';
                     const searchTerm = input.value.toLowerCase();
 
                     const matchingFonts = isFocus && !searchTerm ? 
-                        googleFonts :
+                        googleFonts.slice(0, 350) :
                         googleFonts.filter(font => 
                             font.toLowerCase().includes(searchTerm)
-                        );
+                        ).slice(0, 350);
 
                     suggestionsDiv.innerHTML = '';
                     
